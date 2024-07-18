11b8b9a836b5da2b9698408cac11490e9fc48c4a
johnoro <officialjohnorourke@gmail.com>
2024-07-17 23:50:45 -0400

    adds drag n drop functionality

--- js/components/Button.js
+++ js/components/Button.js
@@ -2,6 +2,33 @@ import Image from './Image.js';
 
 const imgDiv = document.querySelector('.imgs');
 
+// Drag n' Drop / sorting functionality
+let draggedImg = null;
+imgDiv.addEventListener('dragstart', e => {
+	draggedImg = e.target;
+	e.dataTransfer.effectAllowed = 'move';
+});
+imgDiv.addEventListener('dragover', e => {
+	e.preventDefault();
+});
+const indexOfImg = img => {
+	return Array.prototype.indexOf.call(imgDiv.children, img) + 1;
+};
+const swapImgs = (a, b, aInd) => {
+	imgDiv.insertBefore(a, b);
+	imgDiv.insertBefore(b, imgDiv.children[aInd]);
+	imgDiv.appendChild;
+};
+imgDiv.addEventListener('drop', e => {
+	e.preventDefault();
+	if (e.target.tagName === 'IMG' && e.target !== draggedImg) {
+		const draggedInd = indexOfImg(draggedImg);
+		const targetInd = indexOfImg(e.target);
+		if (draggedInd > targetInd) swapImgs(draggedImg, e.target, draggedInd);
+		else swapImgs(e.target, draggedImg, targetInd);
+	}
+});
+
 const ctaHeight = document.querySelector('.cta').getBoundingClientRect().height;
 const isViewFull = () => {
 	const currHeight = imgDiv.getBoundingClientRect().height;
